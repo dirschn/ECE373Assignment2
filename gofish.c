1f131658f4842a5f559f90183cb9b85ef61b16fd
dirschn <dirschn@gmail.com>
2018-10-13 22:40:05 +0000

    Add files via upload

--- gofish.c
+++ gofish.c
@@ -6,42 +6,41 @@
 
 
 
-int main(int args, char* argv[]) 
-{
-  shuffle();
-char yesOrNo;
+int main(int args, char* argv[]) {
+    while (true) {
+        shuffle();
+        char yesOrNo;
 
 
-  while(!game_over(&user) || !game_over(&computer)){
-      printUsers();
-      user_play(&user);
-      printUsers();
-      computer_play(&computer);
+        while (!game_over(&user) || !game_over(&computer)) {
+            printUsers();
+            user_play(&user);
+            printUsers();
+            computer_play(&computer);
 
-  }
+        }
 
-  if(game_over(&user)){
-      printf("Player %d Wins! %d-%d", user.player_number, user.book_size, computer.book_size);
-  }
+        if (game_over(&user)) {
+            printf("Player %d Wins! %d-%d", user.player_number, user.book_size, computer.book_size);
+        } else if (game_over(&computer)) {
+            printf("Player %d Wins! %d-%d", computer.player_number, computer.book_size, user.book_size);
 
-  else if(game_over(&computer)){
-      printf("Player %d Wins! %d-%d", computer.player_number, computer.book_size, user.book_size);
+        }
 
-  }
-
-  printf("Do you want to play again [Y/N]: ");
-  scanf("%c", &yesOrNo);
-  _Bool finish=1;
-  while(finish) {
-      if (yesOrNo == 'Y') {
-        finish=0;
-
-      } else if (yesOrNo == 'N') {
-          printf("Exiting.");
-          exit(0);
-      } else
-          printf("Please enter 'Y' or 'N'.");
-  }
+        printf("Do you want to play again [Y/N]: ");
+        scanf("%c", &yesOrNo);
+        _Bool finish = 1;
+        while (finish) {
+            if (yesOrNo == 'Y') {
+                finish = 0;
+                resetGame();
+            } else if (yesOrNo == 'N') {
+                printf("Exiting.");
+                exit(0);
+            } else
+                printf("Please enter 'Y' or 'N'.");
+        }
+    }
 }
 
 
@@ -49,4 +48,9 @@ int printUsers(){
     print_card_list(&user);
     print_book(&user);
     print_book(&computer);
+}
+
+int resetGame(){
+    reset_player(&user);
+    reset_player(&computer);
 }
