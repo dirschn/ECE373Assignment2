3402c8f503fbf5a6251893421cc7def62be9945c
dirschn <dirschn@gmail.com>
2018-10-13 22:34:04 +0000

    Add files via upload

--- gofish.c
+++ gofish.c
@@ -8,8 +8,45 @@
 
 int main(int args, char* argv[]) 
 {
-  fprintf(stdout, "Put your code here.\n");
   shuffle();
+char yesOrNo;
 
 
+  while(!game_over(&user) || !game_over(&computer)){
+      printUsers();
+      user_play(&user);
+      printUsers();
+      computer_play(&computer);
+
+  }
+
+  if(game_over(&user)){
+      printf("Player %d Wins! %d-%d", user.player_number, user.book_size, computer.book_size);
+  }
+
+  else if(game_over(&computer)){
+      printf("Player %d Wins! %d-%d", computer.player_number, computer.book_size, user.book_size);
+
+  }
+
+  printf("Do you want to play again [Y/N]: ");
+  scanf("%c", &yesOrNo);
+  _Bool finish=1;
+  while(finish) {
+      if (yesOrNo == 'Y') {
+        finish=0;
+
+      } else if (yesOrNo == 'N') {
+          printf("Exiting.");
+          exit(0);
+      } else
+          printf("Please enter 'Y' or 'N'.");
+  }
+}
+
+
+int printUsers(){
+    print_card_list(&user);
+    print_book(&user);
+    print_book(&computer);
 }
